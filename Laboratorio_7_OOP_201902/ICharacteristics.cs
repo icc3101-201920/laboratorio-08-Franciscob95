2bcf34827b2d1a3408cbd786b1b7adba39d0b73c
Franciscob95 <fjbarros1@miuandes.cl>
2019-10-18 10:16:16 -0300

    first commit

---
+++ Laboratorio_7_OOP_201902/ICharacteristics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_7_OOP_201902
+{
+    interface ICharacteristics
+    {
+        List<string> GetCharacteristics();
+    }
+}
---
+++ Laboratorio_7_OOP_201902/Program.cs
@@ -0,0 +1,32 @@
+﻿using Laboratorio_7_OOP_201902.Cards;
+using Laboratorio_7_OOP_201902.Static;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Laboratorio_7_OOP_201902
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Game.txt";
+            if (Directory.Exists(path) == true)
+            {
+                Console.WriteLine("¿Desea cargar la partida anterior? /n 1 = Si /n 2 = No");
+                string answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        game.LoadData(game);
+                        break;
+                case "2":
+                        break;
+                }
+
+            }
+            game.Play();
+        }            
+    }
+}
